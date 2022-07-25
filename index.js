5b11c189cc09c58059c7d616848d0321c5dfa76c
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-25 16:39:48 -0300

    doc: writing comments in add method

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const Node = require('./node');
 
 class BinarySearchTree {
+  // `root` points to the root node, top of the tree,
+  // initially starting with null.
   constructor() { this.root = null }
 
   add(data) {
+    // we reference the root/top node.
     const node = this.root;
 
-    if (node === null) {
+    if (!node) {
+    // if this is the first data storaged, the root node will be null.
+    // then we storage the data right there.
       this.root = new Node(data);
       return;
     } else {
+    // if the first node is already keeping data,
+    // we have to find the right place to store the data in question
       const search = (node) => {
         if (data < node.data) {
           if (node.left === null) {
