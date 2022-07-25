ccebad8c6426cc5abe95dc6feb6dc64bd87a5289
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-25 16:32:15 -0300

    doc: write comments at node.js in Node class

--- index.js
+++ index.js
@@ -34,3 +34,5 @@ class BinarySearchTree {
     }
   }
 }
+
+module.exports = BinarySearchTree;
--- node.js
+++ node.js
@@ -1,6 +1,9 @@
 class Node {
   constructor(data, left = null, right = null) {
+    // `data` is the data that we're trying to storage
     this.data = data;
+    // `left` and `right` are pointing to the
+    // left node and right node of the actual tree.
     this.left = left;
     this.right = right;
   }
